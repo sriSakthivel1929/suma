84cbff66ce1f70d2d066e0e1b378c55ff79acc10
sriSakthivel1929 <skyboisakk@gmail.com>
2026-02-13 22:54:41 +0530

    Update config.js

--- bemyvalentine/src/config.js
+++ bemyvalentine/src/config.js
@@ -1,4 +1,5 @@
-import userConfig from "../config.json" with { type: "json" };
-
-// Export the user configuration directly
-export const config = userConfig;
+export async function loadConfig() {
+  const response = await fetch("/config.json");
+  const config = await response.json();
+  return config;
+}
